45028f155b97e7ee0b64cfb8c1b3dc030a11dbfb
ZHAOzn <453444809@qq.com>
2023-04-01 10:41:25 +0800

    @20230401 修改定时任务周期

--- app/schedule/checkDevDaytime.ts
+++ app/schedule/checkDevDaytime.ts
@@ -5,8 +5,8 @@ export default class checkDev extends Subscription {
     // 通过 schedule 属性来设置定时任务的执行间隔等配置
     static get schedule() {
         return {
-            interval: '0.5m', // 15 分钟间隔
-            // cron: '0 0/21 5,6,7,8,9,10,11 * * *',
+            // interval: '0.5m', // 15 分钟间隔
+            cron: '0 0/21 5,6,7,8,9,10,11 * * *',
             type: 'worker', // 指定随机一个 worker 执行
         };
     }
@@ -15,7 +15,7 @@ export default class checkDev extends Subscription {
     // subscribe 是真正定时任务执行时被运行的函数
     async subscribe() {
         const {ctx} = this;
-        // await ctx.service.dev.handleDevData('en')
+        await ctx.service.dev.handleDevData('en')
         await ctx.service.dev.handleDevData('zh')
     }
 
